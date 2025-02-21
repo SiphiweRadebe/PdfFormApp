eca43cc8d29df0a6d4f431a76a20495cf70d65e7
Yepski <siphiwemoeketsi1@gmail.com>
2025-02-21 14:24:28 +0200

    Add project files.

---
+++ PdfFormApp/Models/FormData.cs
@@ -0,0 +1,9 @@
+﻿namespace PdfFormApp.Models
+{
+    public class FormData
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
